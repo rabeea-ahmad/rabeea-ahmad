40ca11f5cb14da3d2dd9e6f426e446ce88973645
Rabeea Ahmad <ra07@hotmail.ca>
2018-04-03 23:46:37 -0400

    Add about, photos and music sections

--- app.js
+++ app.js
@@ -4,27 +4,4 @@ angular
 
     var vm = this;
 
-    vm.category = {
-      hiking: 'hiking',
-      school: 'school',
-      datascience: 'data science'
-    };
-
-    vm.display = display;
-
-    function display(type) {
-      switch(type) {
-        case vm.category.hiking:
-          //alert('display the hiking photos');
-          break;
-        case vm.category.school:
-          alert('display the school');
-          break;
-        case vm.category.datascience:
-          alert('display data science stuff');
-          break;
-      }
-    }
-
-
   });
